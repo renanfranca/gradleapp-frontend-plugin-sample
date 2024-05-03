face140425f703d0457794eca652c8d817d3da0e
Renan Meneses de Andrade Franca <renan.andradefranca@gmail.com>
2024-05-03 11:55:02 -0300

    Apply git-information module

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
   jacoco
   alias(libs.plugins.spring.boot)
   alias(libs.plugins.jib)
+  alias(libs.plugins.git.properties)
   // jhipster-needle-gradle-plugins
 }
 
@@ -126,6 +127,12 @@ jib {
     permissions = mapOf("/entrypoint.sh" to "755")
   }
 }
+
+gitProperties {
+  failOnNoGitDirectory = false
+  keys = listOf("git.branch", "git.commit.id.abbrev", "git.commit.id.describe", "git.build.version")
+}
+
 // jhipster-needle-gradle-plugins-configurations
 
 repositories {
